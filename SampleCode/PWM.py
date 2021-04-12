623415592905576112bf81d7fd5468c73e8c5384
AI Play and Learn <76465869+WrightWuTW@users.noreply.github.com>
2021-04-12 12:13:41 +0800

    Update PWM.py

--- SampleCode/PWM.py
+++ SampleCode/PWM.py
@@ -4,15 +4,15 @@ pwm0 = PWM(Pin.board.PWM0,freq=50,duty=50)
 pwm0.duty(99)
 pwm0.freq(1000)
 
-pwm1 = PWM(Pin.board.PWM1,freq=50,duty=50) 
+pwm1 = PWM(Pin.board.PWM1,freq=200,duty=50) 
 pwm1.duty(80)
 pwm1.freq(1000)
 
-pwm2 = PWM(Pin.board.PWM2,freq=50,duty=50) 
+pwm2 = PWM(Pin.board.PWM2,freq=100,duty=50) 
 pwm2.duty(30)
 pwm2.freq(2000)
    
-pwm3 = PWM(Pin.board.PWM3,freq=50,duty=50) 
+pwm3 = PWM(Pin.board.PWM3,freq=150,duty=50) 
 pwm3.duty(10)
 pwm3.freq(500)
    
